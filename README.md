7f8b58c08bdae2be8fff4283c1acf8f034d82137
vevedh <vevedh@gmail.com>
2023-09-27 08:07:24 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# feathers-mongodb-management
+
+[![Build Status](https://app.travis-ci.com/feathersjs-ecosystem/feathers-mongodb-management.png?branch=master)](https://app.travis-ci.com/feathersjs-ecosystem/feathers-mongodb-management)
+[![Code Climate](https://codeclimate.com/github/feathersjs-ecosystem/feathers-mongodb-management/badges/gpa.svg)](https://codeclimate.com/github/feathersjs-ecosystem/feathers-mongodb-management)
+[![Test Coverage](https://codeclimate.com/github/feathersjs-ecosystem/feathers-mongodb-management/badges/coverage.svg)](https://codeclimate.com/github/feathersjs-ecosystem/feathers-mongodb-management/coverage)
+[![Download Status](https://img.shields.io/npm/dm/feathers-mongodb-management.svg?style=flat-square)](https://www.npmjs.com/package/feathers-mongodb-management)
+
+> Feathers service adapters for managing MongoDB databases, users and collections
+
+## Installation
+
+With NPM `npm install feathers-mongodb-management --save` or Yarn `yarn add feathers-mongodb-management`
+
+> v1.x is expected to work with Feathers v3 (a.k.a Buzzard) and NodeJS v12
+
+> v2.x is expected to work with Feathers v5 (a.k.a Dove) and NodeJS v16
+
+## Documentation
+
+If a large number of users require it a more complete feathers-mongodb-management documentation will be created. For now as the API is pretty straigth forward the example and [tests](https://github.com/feathersjs-ecosystem/feathers-mongodb-management/tree/master/test) should be sufficient to understand how it works.
+
+The initial use case was to simplify data segregation for a SaaS application where each user could belong to different organisations like in GitHub. Indeed, one key aspect of access control is to filter the resources a user can reach. In this specific use case the users always work in the context of an organisation acting as a filter applied on the manipulated resources. This issue is often tackled by applying a client-side filter on a global service like `/users?org=orgId` and storing on each resouce the organisation it belongs to. We consider this approach as more harder to maintain in the long-term due to additional filtering and relations in the data model. As a consequence we created this plugin to "physically" separate the data of each organisation in a dedicated DB instead of doing this "logically" using filtering.
+
+You can find more information by reading this [article](https://blog.feathersjs.com/access-control-strategies-with-feathersjs-72452268739d).
+
+## Complete Example
+
+Here's an example of a Feathers server that uses `feathers-mongodb-management`.
+
+```js
+const feathers = require('feathers');
+const rest = require('feathers-rest');
+const hooks = require('feathers-hooks');
+const bodyParser = require('body-parser');
+const errorHandler = require('feathers-errors/handler');
+const mongodb = require('mongodb');
+const plugin = require('feathers-mongodb-management');
+
+// Initialize the application
+const app = feathers()
+  .configure(rest())
+  .configure(hooks())
+  // Needed for parsing bodies (login)
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(errorHandler());
+
+// Connect to Mongo instance
+const client = await mongodb.MongoClient.connect('mongodb://127.0.0.1:27017')
+// Initialize your feathers plugin to manage databases
+app.use('/mongo/databases', plugin.database({ adminDb: client.db('feathers-test').admin(), client }));
+let dbService = app.service('/mongo/databases');
+// Now create a new database
+let db = await dbService.create({ name: 'test-db' })
+// The objects provided through the plugin services are just metadata and not MongoDB driver instances
+// We need to retrieve it to create collection/user services that require the DB instance
+db = client.db('test-db');
+// Now create services binded to this database to manage collections/users
+app.use('/mongo/test-db/collections', plugin.collection({ db }));
+let collectionService = app.service('/mongo/test-db/collections');
+const collection = await collectionService.create({ name: 'test-collection' })
+app.use('/mongo/test-db/users', plugin.user({ db }));
+let userService = app.service('/mongo/test-db/users');
+const user = await userService.create({ name: 'test-user', password: 'test-password', roles: ['readWrite'] })
+// Perform other operations using these services if required
+...
+// Then start the app
+app.listen(3030);
+console.log('Feathers app started on 127.0.0.1:3030');
+```
+
+## License
+
+Copyright (c) 2016
+
+Licensed under the [MIT license](LICENSE).
---
+++ test/index.test.js
@@ -0,0 +1,165 @@
+import chai, { util, expect } from 'chai';
+import chailint from 'chai-lint';
+import feathers from '@feathersjs/feathers';
+import configuration from '@feathersjs/configuration';
+import { MongoClient } from 'mongodb';
+import makeDebug from 'debug';
+
+import plugin from '../lib/index.js';
+
+const debug = makeDebug('feathers-mongodb-management:tests');
+
+describe('feathers-mongodb-management', () => {
+  let app, client, adminDb, testDb, databaseService, collectionService, userService;
+
+  before(async () => {
+    chailint(chai, util);
+    app = feathers();
+    // Load app configuration first
+    app.configure(configuration());
+    const url = app.get('db').url;
+    client = await MongoClient.connect(url);
+    adminDb = client.db('feathers-test').admin();
+    await adminDb.listDatabases();
+  });
+
+  it('is CommonJS compatible', () => {
+    expect(typeof plugin).to.equal('function');
+    expect(typeof plugin.database).to.equal('function');
+    expect(typeof plugin.database.Service).to.equal('function');
+    expect(typeof plugin.collection).to.equal('function');
+    expect(typeof plugin.collection.Service).to.equal('function');
+    expect(typeof plugin.user).to.equal('function');
+    expect(typeof plugin.user.Service).to.equal('function');
+  });
+
+  it('registers the plugin', () => {
+    app.configure(plugin);
+  });
+
+  it('creates the database service', () => {
+    app.use('databases', plugin.database({
+      adminDb, client
+    }));
+    databaseService = app.service('databases');
+    expect(databaseService).toExist();
+  });
+
+  it('creates a database', async () => {
+    const db = await databaseService.create({
+      name: 'test-db'
+    });
+    debug(db);
+    testDb = client.db('test-db');
+    expect(testDb).toExist();
+  });
+
+  it('finds databases', async () => {
+    const serviceDbs = await databaseService.find({
+      query: { $select: ['name', 'collections'] }
+    });
+    debug(serviceDbs);
+    const dbsInfo = await adminDb.listDatabases();
+    expect(serviceDbs.length).to.equal(dbsInfo.databases.length);
+    serviceDbs.forEach(db => expect(db.collections).toExist());
+    // Provided by default if no $select
+    serviceDbs.forEach(db => expect(db.objects).beUndefined());
+  });
+
+  it('creates the collection service', () => {
+    app.use('collections', plugin.collection({
+      db: testDb
+    }));
+    collectionService = app.service('collections');
+    expect(collectionService).toExist();
+  });
+
+  it('creates a collection', async () => {
+    const collection = await collectionService.create({
+      name: 'test-collection'
+    });
+
+    debug(collection);
+    // Need to use strict mode to ensure the delete operation has been taken into account
+    const createdCollection = await testDb.collection('test-collection', { strict: true });
+
+    expect(createdCollection).toExist();
+  });
+
+  it('finds collections', async () => {
+    const serviceCollections = await collectionService.find({
+      query: { $select: ['name', 'count'] }
+    });
+    debug(serviceCollections);
+    const collections = await testDb.collections();
+    expect(serviceCollections.length).to.equal(collections.length);
+    serviceCollections.forEach(collection => expect(collection.count).toExist());
+    // Provided by default if no $select
+    serviceCollections.forEach(collection => expect(collection.size).beUndefined());
+  });
+
+  it('removes a collection', async () => {
+    const collection = await collectionService.remove('test-collection');
+
+    debug(collection);
+
+    const collections = await testDb.collections();
+
+    expect(!collections.includes('test-collection'));
+  });
+
+  it('creates the user service', () => {
+    app.use('users', plugin.user({
+      // To test fallback for Mongo <= 2.4
+      // hasUserInfosCommand: false,
+      db: testDb
+    }));
+    userService = app.service('users');
+    expect(userService).toExist();
+  });
+
+  it('creates a user', async () => {
+    const serviceUser = await userService.create({
+      name: 'test-user',
+      password: 'test-password',
+      roles: ['readWrite']
+    });
+    debug(serviceUser);
+    const user = await testDb.command({ usersInfo: 'test-user' });
+    expect(user).toExist();
+  });
+
+  it('finds users', async () => {
+    const serviceUsers = await userService.find({
+      query: { $select: ['name', 'roles'] }
+    });
+    debug(serviceUsers);
+    const data = await testDb.command({ usersInfo: 1 });
+    expect(serviceUsers.length).to.equal(data.users.length);
+    serviceUsers.forEach(user => expect(user.name).toExist());
+    // Provided by default if no $select
+    serviceUsers.forEach(user => expect(user.db).beUndefined());
+  });
+
+  it('removes a user', async () => {
+    const serviceUser = await userService.remove('test-user');
+    debug(serviceUser);
+    try {
+      await testDb.command({ usersInfo: 'test-user' });
+    } catch (error) {
+      expect(error).toExist();
+    }
+  });
+
+  it('removes a database', async () => {
+    const db = await databaseService.remove('test-db');
+    debug(db);
+    const dbsInfo = await adminDb.listDatabases();
+    expect(dbsInfo.databases.find(item => item.name === 'test-db')).beUndefined();
+  });
+
+  // Cleanup
+  after(() => {
+    client.close();
+  });
+});
